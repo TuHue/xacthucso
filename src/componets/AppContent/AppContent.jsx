d1e1842c053d6340646a76bcecf1d0dbaa83be16
TuHue <94226385+TuHue@users.noreply.github.com>
2022-01-27 02:39:28 +0700

    update code

--- src/componets/AppContent/AppContent.jsx
+++ src/componets/AppContent/AppContent.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import {  Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+
 
 //routes config
 import routes from "../../routes";
 
 const AppContent = () => {
     return (
-        <Switch>
-            {routes.map((route, idx) => {
-                return (
-                    route.component && (
-                        <Route
-                            key={idx}
-                            path={route.path}
-                            exact={route.exact}
-                            name={route.name}
-                            render={(props) => (
-                                <>
-                                    <route.component {...props} />
-                                </>
-                            )}
-                        />
+            <Switch>
+                {routes.map((route, idx) => {
+                    return (
+                        route.component && (
+                            <Route
+                                key={idx}
+                                path={route.path}
+                                exact={route.exact}
+                                name={route.name}
+                                render={(props) => (
+                                    <>
+                                        <route.component {...props} />
+                                    </>
+                                )}
+                            />
+                        )
                     )
-                )
-            })}
-        </Switch>
+                })}
+            </Switch>
     )
 }
 export default AppContent
