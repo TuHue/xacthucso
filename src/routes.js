81d2fc12b3673bf5421abb50af7499e6ed9320d3
tuanp20 <phamtuancules20@gmail.com>
2022-01-27 07:38:48 +0700

    lish nhom nguoi dung

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const NguoiDung = React.lazy(()=>import("./views/quanly/quanlynguoidung/NguoiDung/NguoiDung"));
+const NhomNguoiDung = React.lazy(()=>import("./views/quanly/quanlynguoidung/NhomNguoiDung/NhomNguoiDung"));
 
 const routes = [
-    {path: '/quan-tri/quan-tri-nguoi-dung/nguoi-dung',name:"Người Dùng",component:NguoiDung}
+    {path: '/quan-tri/quan-tri-nguoi-dung/nguoi-dung',name:"Người Dùng",component:NguoiDung},
+    {path: '/quan-tri/quan-tri-nguoi-dung/nhom-nguoi-dung',name:"Nhóm người Dùng",component:NhomNguoiDung}
 ]
 
 export default routes;
--- src/views/pages/DangNhap/DangNhap.jsx
+++ src/views/pages/DangNhap/DangNhap.jsx
@@ -1,8 +1,40 @@
 import React from "react"
 
 const DangNhap =()=>{
-    return(
-        <h1>Đăng Nhập</h1>
-    )
+        const [open, setOpen] = useState(false);
+        const handleButtonModal = () => {
+            setOpen(!open);
+        };
+        return (
+            <>
+                <button onClick={handleButtonModal}>Đăng nhập</button>
+                <Modal
+                    isOpen={open}
+                    shouldCloseOnOverlayClick={!open}
+                    className="modal-style">
+                    <div className="modal-page">
+                        <div className="modal-header">
+                            <img src={logo} width="165" height="98" alt="" />
+                            
+                            <h2>Chào mừng bạn đến với Xác Thực Số!</h2>
+                        </div>
+                        <form action="">
+                            <div className="content">
+                                <Input type={'text'} name={'username'} placeholder={'Nhập tài khoản'} display_label={true} children_label={'Tài khoản'} />
+                                <Input type={'password'} name={'password'} placeholder={'Nhập mật khẩu'} display_label={true} children_label={'Mật khẩu'} />
+                                <div className="modal-check">
+                                <Switch defaultChecked />
+                                    <span className="modal-member">Duy trì đănh nhập</span>
+                                </div>
+                                <div className="modal-btn">
+                                    <Button className="modal-btn-login" children={'ĐĂNG NHẬP'} />
+                                    <span className="modal-member">Bạn không nhớ mật khẩu? <Link to="/" className="modal-link">Khôi phục</Link></span>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </Modal>
+            </>
+        );
 }
 export default DangNhap;
